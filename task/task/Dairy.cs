0609ec3bbfe75a7c9c99e4e0fb596eab976d70bf
Kenan <kananmh@code.edu.az>
2023-02-16 02:38:55 +0400

    task

---
+++ task/task/Dairy.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace task
+{
+    internal class Dairy:Product
+    {
+        public double FatPercent;
+    }
+}
---
+++ task/task/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace task
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Drink drink1 = new Drink { AlcoholPercent= 25 };
+            Drink drink2 = new Drink { AlcoholPercent= 39 };
+            Dairy dairy1 = new Dairy { FatPercent= 7 };
+            Dairy dairy2 = new Dairy { FatPercent= 9.8 };
+
+            Store store = new Store();
+
+            store.AddProduct(drink1);
+            store.AddProduct(drink2);
+            store.AddProduct(dairy1);
+            store.AddProduct(dairy2);
+
+            Console.WriteLine(store.AlcoholPercentLimit());
+            Console.WriteLine(store.DairyProductCountLimit());
+
+        }
+    }
+}
